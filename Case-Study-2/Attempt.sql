a854e8977c25030c76eff0ce24298268ed4c4d33
youssefmels <85249386+youssefmels@users.noreply.github.com>
2025-01-06 21:09:53 +0200

    Update Attempt.sql

--- Case-Study-2/Attempt.sql
+++ Case-Study-2/Attempt.sql
@@ -48,6 +48,9 @@ SELECT DATE_PART('DOW', order_time) as Day_of_week, count(c.pizza_id) as Total_P
 
 --            B. Runner and Customer Experience
 -- 1. How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+SELECT
+	DATE_TRUNC('week', registration_date) + INTERVAL '4 days' as Week, COUNT(runner_id) from runners
+   group by DATE_TRUNC('week', registration_date) + INTERVAL '4 days';
 -- 2. What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
 -- 3. Is there any relationship between the number of pizzas and how long the order takes to prepare?
 -- 4. What was the average distance travelled for each customer?
