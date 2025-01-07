e6e56faee16b9366194abe125a3889c3d68042b1
youssefmels <85249386+youssefmels@users.noreply.github.com>
2025-01-07 23:23:16 +0200

    Update Attempt.sql

--- Case-Study-2/Attempt.sql
+++ Case-Study-2/Attempt.sql
@@ -86,3 +86,38 @@ CASE
     	END)/count(order_id)::numeric * 100,0) as Successful_Deliveries_Percentage
 from runner_orders 
 group by runner_id
+
+--	C. Ingredient Optimisation
+-- 1. What are the standard ingredients for each pizza?
+-- 2. What was the most commonly added extra?
+-- 3. What was the most common exclusion?
+-- 4. Generate an order item for each record in the customers_orders table in the format of one of the following:
+--	Meat Lovers
+--	Meat Lovers - Exclude Beef
+--	Meat Lovers - Extra Bacon
+--	Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
+-- 5. Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
+--	For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
+-- 6. What is the total quantity of each ingredient used in all delivered pizzas sorted by most frequent first?
+	
+--	D. Pricing and Ratings
+-- 1. If a Meat Lovers pizza costs $12 and Vegetarian costs $10 and there were no charges for changes - how much money has Pizza Runner made so far if there are no delivery fees?
+-- 2. What if there was an additional $1 charge for any pizza extras?
+--	Add cheese is $1 extra
+-- 3. The Pizza Runner team now wants to add an additional ratings system that allows customers to rate their runner, how would you design an additional table for this new dataset
+-- - generate a schema for this new table and insert your own data for ratings for each successful customer order between 1 to 5.
+-- 4. Using your newly generated table - can you join all of the information together to form a table which has the following information for successful deliveries?
+--	customer_id
+--	order_id
+--	runner_id
+--	rating
+--	order_time
+--	pickup_time
+--	Time between order and pickup
+--	Delivery duration
+--	Average speed
+--	Total number of pizzas
+-- 5. If a Meat Lovers pizza was $12 and Vegetarian $10 fixed prices with no cost for extras and each runner is paid $0.30 per kilometre traveled - how much money does Pizza Runner have left over after these deliveries?
+
+	--E. Bonus Questions
+-- If Danny wants to expand his range of pizzas - how would this impact the existing data design? Write an INSERT statement to demonstrate what would happen if a new Supreme pizza with all the toppings was added to the Pizza Runner menu?
